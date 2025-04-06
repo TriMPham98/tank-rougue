d6782eb447753c3b10b1876bc986e57a105833e8
Tri <trimpham98@gmail.com>
2025-04-05 19:08:53 -0700

    Reduce level requirement for secondary weapons for debug mode

--- src/models/SniperRifle.tsx
+++ src/models/SniperRifle.tsx
@@ -6,7 +6,6 @@ import { useGameState, SecondaryWeapon } from "../utils/gameState"; // Adjust pa
 import { debug } from "../utils/debug";
 import SniperProjectile from "./SniperProjectile";
 import { useWeaponTracking } from "../utils/weaponTracking";
-import { useSound } from "../utils/sound";
 
 // --- UPDATED PROPS INTERFACE ---
 interface SniperRifleProps {
@@ -34,7 +33,6 @@ const SniperRifle = ({
 
   const isPaused = useGameState((state) => state.isPaused);
   const isGameOver = useGameState((state) => state.isGameOver);
-  const sound = useSound();
 
   // Use the shared weapon tracking logic
   const { instanceId } = useWeaponTracking({
@@ -52,8 +50,6 @@ const SniperRifle = ({
         targetId: targetId,
         damage: damage,
       });
-      // Play sniper shot sound when firing
-      sound.play("sniperShot");
     },
   });
 
