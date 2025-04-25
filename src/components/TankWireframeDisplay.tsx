014da68c391d9595997bb51bb81d8022f231cc18
Tri <trimpham98@gmail.com>
2025-04-24 17:46:13 -0700

    Adjust camera and tank wireframe position

--- src/components/TankWireframeDisplay.tsx
+++ src/components/TankWireframeDisplay.tsx
@@ -15,7 +15,7 @@ const TankWireframeDisplay: React.FC<TankWireframeDisplayProps> = ({
   return (
     <div style={{ width, height }}>
       <Canvas
-        camera={{ position: [4, 3, 6], fov: 35 }}
+        camera={{ position: [7.5, 0.5, 0], fov: 35 }}
         style={{
           background: "rgba(0, 0, 0, 0.2)",
           borderRadius: "8px",
@@ -23,14 +23,16 @@ const TankWireframeDisplay: React.FC<TankWireframeDisplayProps> = ({
         }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-        <TankWireframe />
+        <group position={[0, -0.7, 0]}>
+          <TankWireframe />
+        </group>
         <OrbitControls
           enableZoom={false}
           enablePan={false}
           autoRotate={true}
           autoRotateSpeed={1.5}
-          minPolarAngle={Math.PI / 5}
-          maxPolarAngle={Math.PI / 2.5}
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
         />
       </Canvas>
     </div>
